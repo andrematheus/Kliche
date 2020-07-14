ff05f6e659977cdee1172d6ec91c56c5bdecdd6f
André Roque Matheus <amatheus@ligpo.dev>
2020-07-14 07:01:23 -0300

    Groups compiler tests

--- test/features/compilers/SiteTransformsMarkdownFiles.kt
+++ test/features/compilers/SiteTransformsMarkdownFiles.kt
@@ -1,18 +1,22 @@
-package features
+package features.compilers
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import tools.FeatureWithExampleSite
 
-class SiteTransformsJadeFiles : FeatureWithExampleSite("/site-with-jade-files") {
+class SiteTransformsMarkdownFiles : FeatureWithExampleSite("/site-with-markdown-files") {
     @Test
-    internal fun `should compile jade file before returning`() {
+    internal fun `should compile markdown file before returning`() {
         client.withSiteRunning {
             val response = get("/test.html")
             assertEquals(200, response.statusCode)
             assertEquals(
                 //language=HTML
-                "<h1>Title</h1><p><strong>Hello, world!</strong></p>",
+                """
+                <h1>Title</h1>
+                <p><strong>Hello, world!</strong></p>
+                
+                """.trimIndent(),
                 response.text
             )
         }
