79cb957f24537c6325ab4bdb162ad5ab74f72889
André Roque Matheus <amatheus@ligpo.dev>
2020-09-30 01:24:16 -0300

    Add frontend hello world

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -2,6 +2,12 @@ plugins {
     kotlin("js") version "1.4.10"
 }
 
+repositories {
+    maven("https://kotlin.bintray.com/kotlin-js-wrappers/")
+    mavenCentral()
+    jcenter()
+}
+
 kotlin {
     js {
         browser {
@@ -19,14 +25,12 @@ kotlin {
     }
 }
 
-repositories {
-    maven("https://kotlin.bintray.com/kotlin-js-wrappers/")
-    mavenCentral()
-    jcenter()
-}
-
 dependencies {
     implementation(kotlin("stdlib-js"))
+    implementation("org.jetbrains:kotlin-react:16.13.1-pre.105-kotlin-1.3.72")
+    implementation("org.jetbrains:kotlin-react-dom:16.13.1-pre.105-kotlin-1.3.72")
+    implementation(npm("react", "16.13.1"))
+    implementation(npm("react-dom", "16.13.1"))
 
     testImplementation(kotlin("test-js"))
 }
---
+++ frontend/main/Main.kt
@@ -0,0 +1,6 @@
+import kotlinx.browser.document
+
+fun main() {
+    console.log("Será que agora vai?")
+    document.bgColor = "blue"
+}
